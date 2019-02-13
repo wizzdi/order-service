a78ffec2f711bc504d0d0a8a605895a631d099ea
asaf <asaf@flexi-core.com>
2019-02-13 12:38:45 +0200

    init

--- src/main/java/com/flexicore/order/interfaces/IOrderItemRepository.java
+++ src/main/java/com/flexicore/order/interfaces/IOrderItemRepository.java
@@ -19,15 +19,15 @@ import java.util.stream.Collectors;
 
 public interface IOrderItemRepository extends PluginRepository {
 
-    static void addOrderItemPredicates(List<Predicate> preds, CriteriaBuilder cb, Root<OrderItem> r, OrderItemFiltering orderItemFiltering) {
+    static <T extends OrderItem> void addOrderItemPredicates(List<Predicate> preds, CriteriaBuilder cb, Root<T> r, OrderItemFiltering orderItemFiltering) {
         if(orderItemFiltering.getOrders()!=null && !orderItemFiltering.getOrders().isEmpty()){
             Set<String> ids=orderItemFiltering.getOrders().parallelStream().map(f->f.getId()).collect(Collectors.toSet());
-            Join<OrderItem, Order> join=r.join(OrderItem_.order);
+            Join<T, Order> join=r.join(OrderItem_.order);
             preds.add(join.get(Order_.id).in(ids));
         }
         if(orderItemFiltering.getProducts()!=null && !orderItemFiltering.getProducts().isEmpty()){
             Set<String> ids=orderItemFiltering.getProducts().parallelStream().map(f->f.getId()).collect(Collectors.toSet());
-            Join<OrderItem, Product> join=r.join(OrderItem_.product);
+            Join<T, Product> join=r.join(OrderItem_.product);
             preds.add(join.get(Product_.id).in(ids));
         }
     }
--- src/main/java/com/flexicore/order/interfaces/IOrderRepository.java
+++ src/main/java/com/flexicore/order/interfaces/IOrderRepository.java
@@ -19,18 +19,18 @@ import java.util.stream.Collectors;
 
 public interface IOrderRepository extends PluginRepository {
 
-    static void addOrderPredicates(List<Predicate> preds, CriteriaBuilder cb, Root<Order> r, OrderFiltering orderFiltering) {
-        if(orderFiltering.getExternalIds()!=null && !orderFiltering.getExternalIds().isEmpty()){
+    static <T extends Order> void addOrderPredicates(List<Predicate> preds, CriteriaBuilder cb, Root<T> r, OrderFiltering orderFiltering) {
+        if (orderFiltering.getExternalIds() != null && !orderFiltering.getExternalIds().isEmpty()) {
             preds.add(r.get(Order_.externalId).in(orderFiltering.getExternalIds()));
         }
-        if(orderFiltering.getConsumingOrganizations()!=null && !orderFiltering.getConsumingOrganizations().isEmpty()){
-            Set<String> ids=orderFiltering.getConsumingOrganizations().parallelStream().map(f->f.getId()).collect(Collectors.toSet());
-            Join<Order, Organization> join=r.join(Order_.consumingOrganization);
+        if (orderFiltering.getConsumingOrganizations() != null && !orderFiltering.getConsumingOrganizations().isEmpty()) {
+            Set<String> ids = orderFiltering.getConsumingOrganizations().parallelStream().map(f -> f.getId()).collect(Collectors.toSet());
+            Join<T, Organization> join = r.join(Order_.consumingOrganization);
             preds.add(join.get(Organization_.id).in(ids));
         }
-        if(orderFiltering.getSuppliers()!=null && !orderFiltering.getSuppliers().isEmpty()){
-            Set<String> ids=orderFiltering.getSuppliers().parallelStream().map(f->f.getId()).collect(Collectors.toSet());
-            Join<Order, Supplier> join=r.join(Order_.supplier);
+        if (orderFiltering.getSuppliers() != null && !orderFiltering.getSuppliers().isEmpty()) {
+            Set<String> ids = orderFiltering.getSuppliers().parallelStream().map(f -> f.getId()).collect(Collectors.toSet());
+            Join<T, Supplier> join = r.join(Order_.supplier);
             preds.add(join.get(Supplier_.id).in(ids));
         }
     }
